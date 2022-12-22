f163baab1ec20e680e81f5789253c5c6479de714
Seanba <sean@seanba.com>
2022-12-22 17:05:35 -0600

    Add padding to desert summer sprite to avoid seams

--- Assets/SwapTexture/desert.tsx
+++ Assets/SwapTexture/desert.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="Desert" tilewidth="32" tileheight="32" spacing="1" margin="1" tilecount="48" columns="8">
- <image source="desert-summer-tiles.png" width="265" height="199"/>
+<tileset version="1.9" tiledversion="1.9.2" name="Desert" tilewidth="32" tileheight="32" spacing="3" margin="1" tilecount="48" columns="8">
+ <image source="desert-summer-tiles.png" width="279" height="209"/>
  <tile id="30" probability="0.01"/>
  <tile id="31" probability="0.01"/>
  <tile id="37" probability="0.01"/>
